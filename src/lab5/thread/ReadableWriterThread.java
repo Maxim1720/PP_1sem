45b9890ab1a177dbe2196d34ef2207cdab34ea03
Maxim1720 <musiev17@gmail.com>
2023-04-10 02:55:31 +0400

    to refactor 5 lab

--- src/lab5/thread/ReadableWriterThread.java
+++ src/lab5/thread/ReadableWriterThread.java
@@ -1,25 +1,17 @@
 package lab5.thread;
 
 import lab4.readable.ReadableIO;
+import lab5.readable.ReadableWriter;
 
 import java.util.Random;
 
 public class ReadableWriterThread extends Thread{
-
-    private final ReadableIO readableIO;
+    private final ReadableWriter readableWriter;
     public ReadableWriterThread(ReadableIO readableIO){
-        this.readableIO = readableIO;
+        readableWriter = new ReadableWriter(readableIO);
     }
-
-
     @Override
     public void run() {
-        for (int i=0;i<readableIO.authors().length;i++){
-            readableIO.authors()[i] = "";
-            for(int j=0;j<8;j++){
-                readableIO.authors()[i] += (char)new Random().nextInt(65, 122);
-            }
-            System.out.format("Write: %s to position %d%n", readableIO.authors()[i], i);
-        }
+        readableWriter.write();
     }
 }
